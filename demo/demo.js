16bbabefb5c753fe51ac8de73709a251fd7a118a
Tomás Charad <tomas.charad@gmail.com>
2016-01-20 13:28:19 -0300

    Changing sendMessage Id to fit v1.1 of Listener

--- demo/demo.js
+++ demo/demo.js
@@ -55,7 +55,7 @@ function connect(port, baudrate) {
 
 	sp.on("data", function(data) {
 		output.textContent += data.toString();
-		chrome.runtime.sendMessage('jmlkienhpjembdolnhmfeoeongdjoiic', {data: data.toString()});
+		chrome.runtime.sendMessage('mmajeelodhfijncfbkadbofpefcebike', {data: data.toString()});
 	});
 
 	function send() {
